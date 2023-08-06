436b8df94c86d401c059ccdd33bd2f1d988de4cc
ITSC <ITSC@DESKTOP-PMHM7D7>
2023-08-06 09:40:51 +0900

    first commit

---
+++ src/section003/Scan.java
@@ -0,0 +1,17 @@
+package section03;
+
+public class Scan {
+        public static void main(String[] args) {
+        	Scanner scan = new Scanner(System.in);
+			System.out.println("이름, 주소, 나이, 체중을 빈칸으로 구분하여 순서대로 입력하세요.");
+			name = scan.next();
+			address= scan.next();
+			age = scan.nextInt();
+			weight =  scan.nextDouble();
+			
+			System.out.printf("당신의 이름은 %s 입니다. \n",name);
+			System.out.printf("당신의 이름은 %");
+			System.out.printf("당신의 이름은 %");
+			System.out.printf("당신의 이름은 %");
+		}
+}
